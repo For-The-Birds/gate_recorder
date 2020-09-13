5dfb6279d7500f1c580c1a3163cf1dbfd16c79a7
Pavel V <pavel@299792458.ru>
2020-09-13 16:21:52 +0300

    cxxopts

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
 #include <stdlib.h>
 
 #include "gaterecorder.h"
+#include <cxxopts.hpp>
 
 using namespace kfr;
 
-int main(int argc, char ** argv)
+int main(int argc, const char ** argv)
 {
-    float l = -24;
-    if (argc > 1)
-        l = atof(argv[1]);
-    GateRecorder gr(l, atof(argv[2]), atof(argv[3]));
+    cxxopts::Options options("gate_recorder", "Record loud audio frames");
+    options.add_options()
+        ("l,loudness", "Loudness threshold", cxxopts::value<float>()->default_value("18"))
+        ("c,cutoff", "Highpass cutoff freq", cxxopts::value<float>()->default_value("0"))
+        ("r,rolloff", "Highpass rolloff value", cxxopts::value<float>()->default_value("8"))
+        ("o,odir", "Output directory", cxxopts::value<std::string>()->default_value("."))
+        ("q,quiet", "No output", cxxopts::value<bool>()->default_value("false"))
+        ;
+
+    auto o = options.parse(argc, argv);
+
+    chdir(o["o"].as<std::string>().c_str());
+    GateRecorder gr(
+                o["l"].as<float>(),
+                o["c"].as<float>(),
+                o["r"].as<float>());
 
     while(1)
         sleep(100500);
+
     return 0;
 }
